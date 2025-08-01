5c280968900b65d6d231e3800c431fe24847993e
emisayda <unaldiayda@gmail.com>
2025-08-01 17:43:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,6 @@ def cutout():
 
 if __name__ == '__main__':
     os.makedirs('temp', exist_ok=True)
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
